8cb345934b4338ddf6c6ead63c674d9e49fbda90
MoLines Designs <admin@molinesdesigns.com>
2026-05-17 16:28:54 +0200

    bump: brew-tui + brewbar 1.0.0

--- Casks/brewbar.rb
+++ Casks/brewbar.rb
@@ -1,6 +1,6 @@
 cask "brewbar" do
-  version "0.9.2"
-  sha256 "35d38611bdc296d76656a55babfe8e536fc7a9c86ef3951032aa9fbc8b4634b1"
+  version "1.0.0"
+  sha256 "a47cef67d1899c0b08b1f7d179f032164ba9974c8919a43c0994a2925c9bc01f"
 
   url "https://github.com/MoLinesDesigns/Brew-TUI/releases/download/v#{version}/BrewBar.app.zip"
   name "BrewBar"
--- Formula/brew-tui.rb
+++ Formula/brew-tui.rb
@@ -1,8 +1,8 @@
 class BrewTui < Formula
   desc "Visual TUI for Homebrew package management"
   homepage "https://github.com/MoLinesDesigns/Brew-TUI"
-  url "https://registry.npmjs.org/brew-tui/-/brew-tui-0.9.2.tgz"
-  sha256 "06893e3df1ccf816a956b9f229c2d7e6f99e951eb97cc5216b31ff1451c0209d"
+  url "https://registry.npmjs.org/brew-tui/-/brew-tui-1.0.0.tgz"
+  sha256 "50e1815c70d702d697ef2786c9acc214349ad35ce1696aeab669a26bf2c8bb91"
   license "MIT"
 
   depends_on "node@22"
